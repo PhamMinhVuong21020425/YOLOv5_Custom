cd1cb6102d81335377dabe84afa33db9e07ca2b7
PhamMinhVuong21020425 <phamminhv26@gmail.com>
2025-01-14 10:57:04 +0700

    Update to YOLOv5 v6.0

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -14,6 +14,7 @@ from utils.general import colorstr
 
 
 def check_anchor_order(m):
+    # Check anchor order against stride order for YOLOv5 Detect() module m, and correct if necessary
     a = m.anchors.prod(-1).mean(-1).view(-1)  # mean anchor area per output layer
     da = a[-1] - a[0]  # delta a
     ds = m.stride[-1] - m.stride[0]  # delta s
